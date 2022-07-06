ce73491b062dfbf9bbca4fa4148aff36fe9feaea
Suyogya Poudel <soga2058@gmail.com>
2022-07-06 15:40:24 -0600

    User authentication completed. created endpoint that requires user authentication (authorization token) to access.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,8 @@ from users import views
 
 urlpatterns = [
     path('', views.users, name="users"),
-    path('login/', views.login, name="login"),
-    path('register/', views.register, name="register"),
+    path('login/', views.user_login, name="login"),
+    path('register/', views.user_register, name="register"),
+    path('delete_user/', views.Delete.as_view(), name="delete_user"),
     path('delete_all/', views.delete_all, name="delete")
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,21 @@
+# From Django
 from django.http import JsonResponse
-from django.shortcuts import render
 from django.middleware.csrf import get_token
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from .serializer import UserSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate, login, logout
+
+# From rest_framework_simplejwt
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+# From rest_framework
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 
 import json
+from .serializer import UserSerializer
+
+jwt = JWTAuthentication()
 
 # Create your views here.
 def users(request):
@@ -14,22 +23,34 @@ def users(request):
     users = UserSerializer(User.objects.all(), many=True)
     return JsonResponse({'users': list(users.data), 'token': token}, safe=False)
 
-def login(request):
+def user_login(request):
     data = json.loads(request.body)
-    authenticated = authenticate(username=data['username'], password=data['password'])
-    if(authenticated):
-        user = User.objects.get(username=data['username'])
-        refreshToken = RefreshToken.for_user(user)
-        return JsonResponse({"loggedIn": True, "access_token": str(refreshToken.access_token)}, safe=False)
+    user = authenticate(username=data['username'], password=data['password'])
+    if(user):
+        login(request, user)
+        token = RefreshToken.for_user(user)
+        return JsonResponse({"loggedIn": True, "access_token": str(token.access_token)}, safe=False)
     return JsonResponse({"loggedIn": False}, safe=False)
 
-def register(request):
+def user_register(request):
     try:
         data = json.loads(request.body)
-        user = User.objects.create_user(username=data['username'], password=data['password'])
+        user = User.objects.create_user(
+            username=data['username'], 
+            password=data['password'], 
+            email=data['email'])
         return JsonResponse({"success": True}, safe=False)
     except: 
         return JsonResponse({"error": True, "message":" Username already exists"}, safe=False)
+
+
+class Delete(APIView):
+    permission_classes= [IsAuthenticated]
+    def delete(self, request):
+        # jwt.authenticate(request) returns user object and a token
+        user, token = jwt.authenticate(request)
+        return JsonResponse({"success": True}, safe=False)
+
 
 
 def delete_all(request):
