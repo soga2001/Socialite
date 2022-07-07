4fb2ed6373ac65d8744eb2c0612f6aaed7400ca1
Suyogya Poudel <soga2058@gmail.com>
2022-07-06 20:18:26 -0600

    Started front-end. Currently contains a couple of files that came with initializing a vue project.

---
+++ frontend/src/App.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+body {
+  display: flex;
+  place-items: center;
+}
+
+header {
+  display: flex;
+  place-items: center;
+  padding-right: calc(var(--section-gap) / 2);
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  text-align: left;
+  margin-left: -1rem;
+  font-size: 1rem;
+  padding: 1rem 1rem;
+  /* margin-top: 1rem; */
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home">
+    <h1>This is a home page</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.home {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+</style>
