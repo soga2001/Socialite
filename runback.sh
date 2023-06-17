3d394eaf5e17704b500b0f4ae0ebe8283727564c
Suyogya Poudel <soga2058@gmail.com>
2023-06-17 05:17:56 -0600

    Made it possible to run https server using Werkzeug.

--- runback.sh
+++ runback.sh
@@ -1,4 +1,7 @@
 source venv/bin/activate
 cd backend
-python3 manage.py runserver
+# python3 manage.py runserver
+# daphne -b 0.0.0.0 -p 8000 config.asgi:application  -v2
+# python manage.py runserver -v2
+python3 manage.py runserver_plus --cert-file /Users/Suyogya/Projects/BasedBook/localhost.pem --key-file /Users/Suyogya/Projects/BasedBook/localhost-key.pem
 # gunicorn -b localhost:8000 backend.wsgi:application --reload
