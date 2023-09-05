ab9da166d3a6dcc3b441f470ef02c932c12f85cf
soga2001 <soga2058@gmail.com>
2023-09-05 02:39:42 +0000

    Updated data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,6 +111,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'https://localhost:3000',
     'https://127.0.0.1:3000',
+    'https://socialite.suyogyapoudel.com/',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
